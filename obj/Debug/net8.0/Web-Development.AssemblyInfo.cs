8615255f8baf78db5775b4fc72146229b33e1a98
AlexEnabled <114242121+AlexEnabled@users.noreply.github.com>
2024-09-23 23:30:51 +0200

    sdf

--- obj/Debug/net8.0/Web-Development.AssemblyInfo.cs
+++ obj/Debug/net8.0/Web-Development.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web-Development")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fb3888fb495c502d85152afc3925803e55f08f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7456ce2d17a028297e4e630677d3bb1f96b1a29")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web-Development")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web-Development")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
