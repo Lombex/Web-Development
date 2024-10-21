3f0c201e03a2dd66b53d28737342ff439d6b0bd1
AlexEnabled <114242121+AlexEnabled@users.noreply.github.com>
2024-10-21 13:41:21 +0200

    user fixed
    fixed

--- controllers/UserController.cs
+++ controllers/UserController.cs
@@ -8,28 +8,28 @@ public class UserController : ControllerBase {
 
     [HttpPost("create")] // http://localhost:5001/api/user/create
     public async Task<IActionResult> CreateUser([FromBody] User user) {
-        var _user = new User(Guid.NewGuid(), user.Firstname, user.Lastname, user.Email, user.Password, user.RecuringDays);
+        var _user = new User(Guid.NewGuid(), user.Firstname, user.Lastname, user.Email, user.Password, user.RecuringDays, user.Points);
         // Save "_user" to new database. 
         return Ok("User has been sucessfully created!");
     }
 
     [HttpGet()] // Get ID form user for url parm | http://localhost:5001/api/user/{id}
     public async Task<IActionResult> GetUser([FromBody] User user) {
-        var _user = new User(Guid.NewGuid(), user.Firstname, user.Lastname, user.Email, user.Password, user.RecuringDays);
+        var _user = new User(Guid.NewGuid(), user.Firstname, user.Lastname, user.Email, user.Password, user.RecuringDays, user.Points);
         // Get "_user" info from database. 
         return Ok("return user here...");
     }
 
     [HttpGet("all")] // http://localhost:5001/api/user/all
     public async Task<IActionResult> GetAllUsers([FromBody] User user) {
-        var _user = new User(Guid.NewGuid(), user.Firstname, user.Lastname, user.Email, user.Password, user.RecuringDays);
+        var _user = new User(Guid.NewGuid(), user.Firstname, user.Lastname, user.Email, user.Password, user.RecuringDays, user.Points);
         // Get all users from database. 
         return Ok("return user here...");
     }
 
     [HttpPut("update/v1")] // http:localhost:5001/api/user/update/v1
     public async Task<IActionResult> UpdateUser([FromBody] User user) {
-        var _user = new User(user.id, user.Firstname, user.Lastname, user.Email, user.Password, user.RecuringDays);
+        var _user = new User(user.id, user.Firstname, user.Lastname, user.Email, user.Password, user.RecuringDays, user.Points);
         // Update Intire User from "_user" 
         return Ok("User has been successfully updated");
     }
@@ -43,7 +43,7 @@ public class UserController : ControllerBase {
 
     [HttpDelete("delete")] // http:localhost:5001/api/user/delete
     public async Task<IActionResult> DeleteUser([FromBody] User user) {
-        var _user = new User(user.id, user.Firstname, user.Lastname, user.Email, user.Password, user.RecuringDays);
+        var _user = new User(user.id, user.Firstname, user.Lastname, user.Email, user.Password, user.RecuringDays, user.Points);
         // Delete "_user" from database.
         return Ok("User has been succesfully deleted!");
     }
