a2f1b8892d448fc24e5e0fd1a6003fbbd6f0e97a
Altaaf <djallied.d@hotmail.com>
2024-12-03 00:32:28 +0100

    added Login
    added login.tsx

--- frontend/clientapp/src/App.tsx
+++ frontend/clientapp/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import PointShop from '../src/components/shop/PointsShop';
+import Login from './components/auth/login';
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
-      <PointShop />
+      <Login />
     </div>
   );
 }
