0f127b18f69604e39e663e35e9d56abebc6f0c62
Steyn229 <steyn081@hotmail.com>
2024-12-04 12:04:00 +0100

    small fix

--- obj/Debug/net8.0/Web-Development.AssemblyInfo.cs
+++ obj/Debug/net8.0/Web-Development.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web-Development")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+232b121c85b436f39bdc9eb65e7191eb1e227b91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25f090b9cf3ab9ee2bdbec816fb6891aa37e347a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web-Development")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web-Development")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
