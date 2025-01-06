3208f195c464fdf23ce4ce288e77355dbb47e0bc
Steyn229 <steyn081@hotmail.com>
2025-01-06 11:42:41 +0100

    smill fix

--- services/EventService.cs
+++ services/EventService.cs
@@ -1,25 +1,8 @@
-public class EventService
-{
-    private List<Event> _events = new();
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
-    public async Task<Event?> GetEventAsync(Guid id) => await Task.FromResult(_events.FirstOrDefault(e => e.Id == id));
-    public async Task<IEnumerable<Event>> GetAllEventsAsync() => await Task.FromResult(_events);
-    public async Task<Event> CreateEventAsync(Event eventItem) { _events.Add(eventItem); return await Task.FromResult(eventItem); }
-    public async Task<Event?> UpdateEventAsync(Guid id, Event eventItem)
-    {
-        var index = _events.FindIndex(e => e.Id == id);
-        if (index == -1) return null;
-        _events[index] = eventItem with { Id = id };
-        return await Task.FromResult(_events[index]);
-    }
-    public async Task<bool> DeleteEventAsync(Guid id)
-    {
-        var eventItem = _events.FirstOrDefault(e => e.Id == id);
-        if (eventItem == null) return false;
-        _events.Remove(eventItem);
-        return await Task.FromResult(true);
-    }
-}
 public interface IEventService
 {
     Task<Event?> GetEventAsync(Guid id);
@@ -27,4 +10,57 @@ public interface IEventService
     Task<Event> CreateEventAsync(Event eventItem);
     Task<Event?> UpdateEventAsync(Guid id, Event eventItem);
     Task<bool> DeleteEventAsync(Guid id);
+}
+
+public class EventService : IEventService
+{
+    private readonly AppDbContext _context;
+
+    public EventService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    // Haal een event op via ID
+    public async Task<Event?> GetEventAsync(Guid id)
+    {
+        return await _context.Events.FindAsync(id); 
+    }
+
+    // Haal alle events op
+    public async Task<IEnumerable<Event>> GetAllEventsAsync()
+    {
+        return await _context.Events.ToListAsync(); 
+    }
+
+    // Maak een nieuw event aan
+    public async Task<Event> CreateEventAsync(Event eventItem)
+    {
+        await _context.Events.AddAsync(eventItem); 
+        await _context.SaveChangesAsync(); 
+        return eventItem; 
+    }
+
+    // Update een bestaand event
+    public async Task<Event?> UpdateEventAsync(Guid id, Event eventItem)
+    {
+        var existingEvent = await _context.Events.FindAsync(id); 
+        if (existingEvent == null) return null;
+        _context.Events.Remove(existingEvent); 
+        await _context.Events.AddAsync(new Event(id, eventItem.Title, eventItem.Description, eventItem.StartTime, eventItem.EndTime, eventItem.Location, eventItem.Approval)); // Voeg het bijgewerkte event opnieuw toe
+        await _context.SaveChangesAsync();
+
+        return eventItem; 
+    }
+
+    // Verwijder een event
+    public async Task<bool> DeleteEventAsync(Guid id)
+    {
+        var eventItem = await _context.Events.FindAsync(id); 
+        if (eventItem == null) return false;
+
+        _context.Events.Remove(eventItem);
+        await _context.SaveChangesAsync();
+        return true; 
+}
 }
