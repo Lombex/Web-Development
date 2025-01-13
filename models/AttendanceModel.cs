2ac873834ac5d9cb30be977b4c0c28e0a7826d67
Steyn229 <steyn081@hotmail.com>
2025-01-13 21:09:07 +0100

    voor check

--- models/AttendanceModel.cs
+++ models/AttendanceModel.cs
@@ -2,8 +2,15 @@ using System;
 
 public class Attendance
 {
-    
     public Guid Id { get; set; }
-    public Guid UserId { get; set; }
     public DateTime Date { get; set; }
+
+    // Relatie met EventAttendance
+    public Guid EventAttendanceId { get; set; }
+    public EventAttendance EventAttendance { get; set; }
+
+    // Relatie met User
+    public Guid UserId { get; set; }
+    public User User { get; set; }
 }
+
